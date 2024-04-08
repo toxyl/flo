afac74268dc0310367a66559f8d968b0282b358b
tox <tox@novasonica.com>
2024-04-08 21:51:04 +0200

    - added CR, LF, CRLF, StrUnderline, StrBold, StrItalic methods to utils.String

--- utils/string.go
+++ utils/string.go
@@ -16,9 +16,33 @@ func (s *String) Rune(r rune) *String {
 	return s
 }
 
+func (s *String) CR() *String {
+	return s.Rune('\r')
+}
+
+func (s *String) LF() *String {
+	return s.Rune('\n')
+}
+
+func (s *String) CRLF() *String {
+	return s.CR().LF()
+}
+
 func (s *String) Str(str string) *String {
 	s.builder.WriteString(str)
 	return s
+}
+
+func (s *String) StrUnderline(str string) *String {
+	return s.Str(glog.Underline() + str + glog.Reset())
+}
+
+func (s *String) StrBold(str string) *String {
+	return s.Str(glog.Bold() + str + glog.Reset())
+}
+
+func (s *String) StrItalic(str string) *String {
+	return s.Str(glog.Italic() + str + glog.Reset())
 }
 
 func (s *String) StrClean(clean bool, str string) *String {
