1389e5f7171013f0d469f8d36dbb02fb4ec582b4
tox <tox@novasonica.com>
2024-04-10 20:19:36 +0200

    - fixed errors being thrown when err == nil

--- errors/main.go
+++ errors/main.go
@@ -1,6 +1,7 @@
 package errors
 
 import (
+	"fmt"
 	"io/fs"
 
 	"github.com/toxyl/errors"
@@ -10,21 +11,23 @@ var (
 	ErrNoEncoderImplemented     = errors.Newf("no encoder implemented")
 	ErrNoDecoderImplemented     = errors.Newf("no decoder implemented")
 	ErrChecksumAlgorithmInvalid = func(name string) error { return errors.Newf("%s is not a valid checksum algorithm", name) }
-	ErrFailedToOpenFile         = func(file string, err error) error { return errors.Newf("failed to open file %s: %v", file, err) }
-	ErrFailedToCreateDir        = func(file string, err error) error { return errors.Newf("failed to create dir %s: %v", file, err) }
-	ErrFailedToCreateFile       = func(file string, err error) error { return errors.Newf("failed to create file %s: %v", file, err) }
-	ErrFailedToDeleteFile       = func(file string, err error) error { return errors.Newf("failed to delete %s: %v", file, err) }
-	ErrFailedToCopyFile         = func(src, dst string, err error) error {
-		return errors.Newf("failed copy %s to %s: %v", src, dst, err)
+	ErrFile                     = func(operation, file string, err error) error {
+		if err != nil {
+			return errors.Newf("failed to %s %s", operation, file).Append(err)
+		}
+		return nil
 	}
+	ErrFailedToOpenFile       = func(file string, err error) error { return ErrFile("open file", file, err) }
+	ErrFailedToCreateDir      = func(file string, err error) error { return ErrFile("create dir", file, err) }
+	ErrFailedToCreateFile     = func(file string, err error) error { return ErrFile("create file", file, err) }
+	ErrFailedToDeleteFile     = func(file string, err error) error { return ErrFile("delete", file, err) }
+	ErrFailedToCopyFile       = func(src, dst string, err error) error { return ErrFile(fmt.Sprintf("copy %s to", src), dst, err) }
 	ErrFailedToSetPermissions = func(file string, mode fs.FileMode, err error) error {
-		return errors.Newf("failed set %s to %s: %v", file, mode.String(), err)
+		return ErrFile(fmt.Sprintf("set %s permissions on", mode.String()), file, err)
 	}
 	ErrIsNotExecutable = func(file string) error {
 		return errors.Newf("%s is not an executable, use PermExec(o, g, w) first", file)
 	}
-	ErrIsNotDirectory = func(file string) error {
-		return errors.Newf("%s is not a directory", file)
-	}
-	ErrMustBePointer = func(target any) error { return errors.Newf("expected *%T, but got %T", target, target) }
+	ErrIsNotDirectory = func(file string) error { return errors.Newf("%s is not a directory", file) }
+	ErrMustBePointer  = func(target any) error { return errors.Newf("expected *%T, but got %T", target, target) }
 )
