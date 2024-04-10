7320f18ba6a26def9731f56138e06df53cddaa3f
tox <tox@novasonica.com>
2024-04-10 10:17:13 +0200

    - using github.com/toxyl/errors

--- errors/main.go
+++ errors/main.go
@@ -1,29 +1,30 @@
 package errors
 
 import (
-	"fmt"
 	"io/fs"
+
+	"github.com/toxyl/errors"
 )
 
 var (
-	ErrNoEncoderImplemented     = fmt.Errorf("no encoder implemented")
-	ErrNoDecoderImplemented     = fmt.Errorf("no decoder implemented")
-	ErrChecksumAlgorithmInvalid = func(name string) error { return fmt.Errorf("%s is not a valid checksum algorithm", name) }
-	ErrFailedToOpenFile         = func(file string, err error) error { return fmt.Errorf("failed to open file %s: %v", file, err) }
-	ErrFailedToCreateDir        = func(file string, err error) error { return fmt.Errorf("failed to create dir %s: %v", file, err) }
-	ErrFailedToCreateFile       = func(file string, err error) error { return fmt.Errorf("failed to create file %s: %v", file, err) }
-	ErrFailedToDeleteFile       = func(file string, err error) error { return fmt.Errorf("failed to delete %s: %v", file, err) }
+	ErrNoEncoderImplemented     = errors.Newf("no encoder implemented")
+	ErrNoDecoderImplemented     = errors.Newf("no decoder implemented")
+	ErrChecksumAlgorithmInvalid = func(name string) error { return errors.Newf("%s is not a valid checksum algorithm", name) }
+	ErrFailedToOpenFile         = func(file string, err error) error { return errors.Newf("failed to open file %s: %v", file, err) }
+	ErrFailedToCreateDir        = func(file string, err error) error { return errors.Newf("failed to create dir %s: %v", file, err) }
+	ErrFailedToCreateFile       = func(file string, err error) error { return errors.Newf("failed to create file %s: %v", file, err) }
+	ErrFailedToDeleteFile       = func(file string, err error) error { return errors.Newf("failed to delete %s: %v", file, err) }
 	ErrFailedToCopyFile         = func(src, dst string, err error) error {
-		return fmt.Errorf("failed copy %s to %s: %v", src, dst, err)
+		return errors.Newf("failed copy %s to %s: %v", src, dst, err)
 	}
 	ErrFailedToSetPermissions = func(file string, mode fs.FileMode, err error) error {
-		return fmt.Errorf("failed set %s to %s: %v", file, mode.String(), err)
+		return errors.Newf("failed set %s to %s: %v", file, mode.String(), err)
 	}
 	ErrIsNotExecutable = func(file string) error {
-		return fmt.Errorf("%s is not an executable, use SetExec(o, g, w) first", file)
+		return errors.Newf("%s is not an executable, use SetExec(o, g, w) first", file)
 	}
 	ErrIsNotDirectory = func(file string) error {
-		return fmt.Errorf("%s is not a directory", file)
+		return errors.Newf("%s is not a directory", file)
 	}
-	ErrMustBePointer = func(target any) error { return fmt.Errorf("expected *%T, but got %T", target, target) }
+	ErrMustBePointer = func(target any) error { return errors.Newf("expected *%T, but got %T", target, target) }
 )
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/toxyl/flo
 go 1.22.1
 
 require (
+	github.com/toxyl/errors v0.0.0-20240410073853-96b96b437ed5
 	github.com/toxyl/glog v1.0.0-alpha.15
 	gopkg.in/yaml.v3 v3.0.1
 )
